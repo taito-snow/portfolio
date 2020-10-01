696c31e590668077285393239226d560c5ff56d3
Taito Ito <taito.football@icloud.com>
2020-10-01 10:23:57 +0900

    修正

---
+++ app.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('.fade-img img:not(:first-child)').hide();
+  setInterval(function () {
+    $('.fade-img img:first-child')
+      .fadeOut('slow')
+      .next('img')
+      .fadeIn('slow')
+      .end()
+      .appendTo('.fade-img');
+  }, 3500);
+});
